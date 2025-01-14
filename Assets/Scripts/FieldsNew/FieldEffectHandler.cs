6b61a08e72ee672fe09a42f61d814aa8601934b3
Joshua Wheeler <joshuawheeler15@gmail.com>
2025-01-13 20:57:45 -0700

    Field Effect Audio Source
    FieldEffectHandlers now add an AudioSource component upon initialization and then set that audio source component's mixer group to the Wheel group.

--- Assets/Scripts/FieldsNew/FieldEffectHandler.cs
+++ Assets/Scripts/FieldsNew/FieldEffectHandler.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public abstract class FieldEffectHandler : MonoBehaviour
 {
     public Field FieldData { get; private set; }
     protected WheelSegment Segment { get; private set; }
     protected PlayerStats playerStats;
+    protected AudioSource audioSource;
     //protected SkillStats skillStats;
 
     public virtual void Initialize(Field fieldData)
     {
         FieldData = fieldData;
         playerStats = FindObjectOfType<PlayerStats>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+        AudioMixer mixer = Resources.Load("Master Mixer") as AudioMixer;
+        audioSource.outputAudioMixerGroup = mixer.FindMatchingGroups("Wheel")[0];
     }
 
     public abstract void OnEnter(Player player);
