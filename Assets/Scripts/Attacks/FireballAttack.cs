d13404e89186e408612188b95464a79078bc7946
Sina <sinascribblecraft@gmail.com>
2025-01-20 05:51:57 +0100

    Squashed commit of the following:
    commit 43aa0f15d56beced480f2b408ec5a4fe7dc6639b
    Author: luis <luism.perestrelo@gmail.com>
    Date:   Mon Jan 20 00:44:27 2025 +0000
    
        actually tightened angles now, but they were already 20f and are 15f now. Probably have to go even lower
    
    commit ab8e5158b4c3ef91f7c9bb216a69db06d56f5356
    Author: luis <luism.perestrelo@gmail.com>
    Date:   Mon Jan 20 00:27:51 2025 +0000
    
        ShieldBuster indicator is now green
    
    commit 6b9ffd0aeb4fd605bfe163d89e25b5e3fc7477c7
    Author: luis <luism.perestrelo@gmail.com>
    Date:   Mon Jan 20 00:24:01 2025 +0000
    
        increased Heal cooldown
    
    commit d029cd133da1b4d759fe5febf3c5e4fc9a3c76d2
    Author: luis <luism.perestrelo@gmail.com>
    Date:   Mon Jan 20 00:21:39 2025 +0000
    
        Tightened spreadAngle for multi-proj attacks from 30 to 15
    
    commit e1e8e81ac2803ab01bcbb2481debc82933a6655a
    Author: luis <luism.perestrelo@gmail.com>
    Date:   Mon Jan 20 00:15:00 2025 +0000
    
        Tightened spreadAngle for multi-proj attacks from 30 to 15
    
    commit cd519d67bea34147cd16f804e5b0907da58f127b
    Author: luis <luism.perestrelo@gmail.com>
    Date:   Sun Jan 19 21:41:03 2025 +0000
    
        made fat tentacle angle consider player direction
    
    commit d92ccb90a2afdf83af1a03117d92e0fdc3d3b3a0
    Author: Joshua Wheeler <joshuawheeler15@gmail.com>
    Date:   Sun Jan 19 11:33:01 2025 -0700
    
        Player movement revert & Fireball fix
    
        Player's movement is now back on the default setting and the fireball no longer throws a NRE
    
    commit 0b141fb475fd80e181ec482669dd43b7bbfa3784
    Merge: 6e7f1f38 ef2a1c04
    Author: SinaScribblecraft <sinascribblecraft@gmail.com>
    Date:   Sun Jan 19 19:21:38 2025 +0100
    
        Merge pull request #10 from luisperestrelo/fix-visuals
    
        Fix visuals

--- Assets/Scripts/Attacks/FireballAttack.cs
+++ Assets/Scripts/Attacks/FireballAttack.cs
@@ -5,7 +5,7 @@ public class FireballAttack : BaseAttack
 {
     [SerializeField] private FireballProjectile fireballPrefab;
 
-    public override void PerformAttack(PlayerCombat playerCombat, float fireRate, PlayerStats playerStats, int projectileCount, float spreadAngle = 30f)
+    public override void PerformAttack(PlayerCombat playerCombat, float fireRate, PlayerStats playerStats, int projectileCount, float spreadAngle = 15f)
     {
         base.PerformAttack(playerCombat);
 
