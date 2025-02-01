6f24ed7a6ee09d6877086c12bf8d59ed617b2a05
Joshua Wheeler <joshuawheeler15@gmail.com>
2025-01-31 20:57:01 -0700

    +Hourglass explosion SFX

--- Assets/Scripts/ImpactAnimation.cs
+++ Assets/Scripts/ImpactAnimation.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class ImpactAnimation : MonoBehaviour
 {
+    [SerializeField] private float destroyDelay = 1;
+
+    private Collider2D col;
+
+    private void Awake()
+    {
+        col = GetComponent<Collider2D>();
+    }
 
     public void OnCompleted()
     {
+        if (col != null)
+            col.enabled = false;
+        StartCoroutine(DestroyAfterDelay());
+    }
+    
+    private IEnumerator DestroyAfterDelay()
+    {
+        yield return new WaitForSeconds(destroyDelay);
         Destroy(gameObject);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
