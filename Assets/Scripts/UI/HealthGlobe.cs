2f5be0f13d51b74af20ad2a656925119c66672b0
Sina <sinascribblecraft@gmail.com>
2025-02-01 02:26:11 +0100

    added health globe reduction animation

--- Assets/Scripts/UI/HealthGlobe.cs
+++ Assets/Scripts/UI/HealthGlobe.cs
@@ -1,10 +1,15 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthGlobe : MonoBehaviour
 {
     [SerializeField] private Image fillImage;
+    [SerializeField] private Image whiteFillImage;
     private Health targetHealth;
+
+    private Coroutine reduceFillCoroutine;
+    private Coroutine reduceWhiteCoroutine;
 
     private void Start()
     {
@@ -23,6 +28,50 @@ public class HealthGlobe : MonoBehaviour
     public void OnHealthChanged(float currentHealth, float maxHealth)
     {
         float fillAmount = currentHealth / maxHealth;
+
         fillImage.fillAmount = fillAmount;
+        // if (reduceFillCoroutine != null)
+        // {
+        //     StopCoroutine(reduceFillCoroutine);
+        // }
+        // reduceFillCoroutine = StartCoroutine(ReduceFill(fillAmount));
+        //
+        
+        if (reduceWhiteCoroutine != null)
+        {
+            StopCoroutine(reduceWhiteCoroutine);
+        }
+        reduceWhiteCoroutine = StartCoroutine(ReduceWhite(fillAmount));
+    }
+
+    private IEnumerator ReduceFill(float toFillAmount)
+    {
+        var duration = 2f;
+        var elapsedTime = 0f;
+        
+        var fromFill = fillImage.fillAmount;
+        var fromWhite = whiteFillImage.fillAmount;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+             
+            fillImage.fillAmount = Mathf.Lerp(fromFill, toFillAmount, elapsedTime / duration);
+            yield return null;
+        }
+    }
+    
+    private IEnumerator ReduceWhite(float toFillAmount)
+    {
+        var duration = 1f;
+        var elapsedTime = 0f;
+        
+        var fromWhite = whiteFillImage.fillAmount;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+             
+            whiteFillImage.fillAmount = Mathf.Lerp(fromWhite, toFillAmount, elapsedTime / duration);
+            yield return null;
+        }
     }
 } 
